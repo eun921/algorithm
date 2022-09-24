56a2f16e2a4cad36e0dd6f7cb2d9356903b2918c
eun921 <62167576+eun921@users.noreply.github.com>
2022-09-25 02:04:32 +0900

    220924

--- 스택,큐_주식가격.cpp
+++ 스택,큐_주식가격.cpp
@@ -4,20 +4,21 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    int size=prices.size();
-    vector<int> answer(size);
-    
-    //뒤에서부터 비교
-    for(int i=size-1;i>=0;i--){
-        //일단 최대 시간으로 설정
-        answer[i]=size-1-i;
-        //현재 가격보다 이후 가격이 더 작으면 answer 갱신
-        for(int j=i+1;j<size;j++){
-            if(prices[i]>prices[j]){
-                answer[i]=j-i;
+    vector<int> answer;
+        
+    for(int i=0;i<prices.size();i++){
+        int tmp=0;
+        for(int j=i+1;j<prices.size();j++){
+            if(prices[i]<=prices[j]){
+                tmp++;
+            }
+            else{
+                tmp++;
                 break;
             }
         }
+        answer.push_back(tmp);
     }
+        
     return answer;
 }
