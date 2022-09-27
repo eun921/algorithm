9d0ce1d5d5823a56df50c366b4c4670f08cfe351
eun921 <62167576+eun921@users.noreply.github.com>
2022-09-28 03:47:35 +0900

    220927

--- 힙_디스크컨트롤러.cpp
+++ 힙_디스크컨트롤러.cpp
@@ -5,11 +5,14 @@
 
 using namespace std;
 
-struct cmp{
-    bool operator()(vector<int> a, vector<int> b){
-        return a.at(1)>b.at(1);
+struct cmp
+{
+    bool operator()(vector<int> a,vector<int> b)//작업 소요시간 기준
+    {
+        return a[1]>b[1];
     }
 };
+
 
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
@@ -18,29 +21,33 @@ int solution(vector<vector<int>> jobs) {
     sort(jobs.begin(),jobs.end());
     //최소 힙 선언
     priority_queue<vector<int>,vector<vector<int>>,cmp> pq;
-    //대기 작업이 없고 우선순위 큐가 빌 때까지 반복
+    
+    //작업이 있거나 우선순위 큐가 비지 않으면 반복
     while(i<jobs.size()||!pq.empty()){
-        //대기 작업이 존재하고 
-        //그 작업이 들어오는 시간이 현재 시간보다 작다면
-        //우선순위 큐에 추가
-        if(i<jobs.size()&&time>=jobs[i][0]){
+        
+        //작업이 있고, 작업이 들어온 시간이 현재 시간보다 작거나 같으면
+        //우선순위 큐에 넣기
+        if(i<jobs.size()&&jobs[i][0]<=time){
             pq.push(jobs[i++]);
             continue;
         }
-        //큐가 비어있지 않다면
-        //작업 수행시간 계산
-        //대기시간 계산
+        
+        //우선순위 큐가 비지 않았으면(대기 작업 존재)
+        //최상위 값(작업시간 가장 작은 것)으로
+        //대기 시간 갱신하고(현재 시간-들어온 시간), 현재 시간도 갱신함(현재 시간+소요시간)
         if(!pq.empty()){
             time+=pq.top()[1];
             answer+=time-pq.top()[0];
             pq.pop();
         }
-        //큐가 비어 있다면
-        //다음 작업 시간으로 넘김
+        //우선순위 큐가 비었으면
+        //대기 작업이 없으므로 다음 작업이 들어오는 시간으로 이동함
         else{
             time=jobs[i][0];
         }
     }
     //평균 계산
-    return answer/jobs.size();
+    
+    answer/=jobs.size();
+    return answer;
 }
