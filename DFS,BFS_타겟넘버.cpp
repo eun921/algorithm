eb3ac1f39655fe7a7602a34d18661ec2426fc64d
eun921 <62167576+eun921@users.noreply.github.com>
2022-10-07 19:29:13 +0900

    221007

--- DFS,BFS_타겟넘버.cpp
+++ DFS,BFS_타겟넘버.cpp
@@ -1,23 +1,28 @@
 #include <string>
 #include <vector>
+
 using namespace std;
 
 int answer = 0;
 
-//dfs 재귀 호출
-void dfs(vector<int>&num,int sum,int depth,int target){
-    if(num.size()<=depth){
+void dfs(vector<int> number, int sum, int depth, int target){
+    //탈출조건
+    //정수를 다 더하거나 뺀 후에
+    //타겟 넘버와 sum이 같으면 answer++
+    if(depth==number.size()){
         if(sum==target){
             answer++;
         }
         return;
     }
-    dfs(num,sum+num[depth],depth+1,target);
-    dfs(num,sum+num[depth]*(-1),depth+1,target);
+    dfs(number,sum+number[depth],depth+1,target); //더하는 경우
+    dfs(number,sum-number[depth],depth+1,target); //빼는 경우
 }
 
 int solution(vector<int> numbers, int target) {
+    
     dfs(numbers,numbers[0],1,target);
     dfs(numbers,numbers[0]*(-1),1,target);
+    
     return answer;
 }
